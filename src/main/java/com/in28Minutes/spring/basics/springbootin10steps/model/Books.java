47510cb4a6daf8c582a2ac5fcccb6887a002664e
Naveed <naveedfarouq@hotmail.co.uk>
2018-11-03 15:08:22 +0000

    adding model - books

---
+++ src/main/java/com/in28Minutes/spring/basics/springbootin10steps/model/Books.java
@@ -0,0 +1,38 @@
+package com.in28Minutes.spring.basics.springbootin10steps.model;
+
+public class Books {
+
+    long id;
+    String name;
+    String author;
+
+    public Books(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
